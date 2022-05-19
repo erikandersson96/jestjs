108efe753b927520a965d481c70aed00635859c7
erikandersson96 <erik96.andersson@gmail.com>
2022-05-19 18:54:30 +0000

    Add comment to code

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,6 +1,8 @@
-function addition(num1, num2) {
-    return num1 + num2; 
-}
+// Uncomment this and comment out button.js and button.test.js to run the calculator instead. 
+
+// function addition(num1, num2) {
+//     return num1 + num2; 
+// }
 
 
-module.exports = addition; 
+// module.exports = addition; 
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,27 +1,29 @@
+// Uncomment this and comment out button.js and button.test.js to run the calculator instead. 
+
 /**
  * @jest-environment jsdom
  */
 
-const addition = require("../calc"); 
+// const addition = require("../calc"); 
 
-const subtraction = require("../calc"); 
+// const subtraction = require("../calc"); 
 
-describe("Calculator", () => {
-    describe("Addition function", () => {
-        test("should return 42 for 20 + 22", () => {
-            expect(addition(20, 22)).toBe(42); 
-        }); 
-        test("should return 73 for 42 + 31", () => {
-            expect(addition(42, 31)).toBe(73);
-        });
-    });
-    describe("Subtraction function", () => {
+// describe("Calculator", () => {
+//     describe("Addition function", () => {
+//         test("should return 42 for 20 + 22", () => {
+//             expect(addition(20, 22)).toBe(42); 
+//         }); 
+//         test("should return 73 for 42 + 31", () => {
+//             expect(addition(42, 31)).toBe(73);
+//         });
+//     });
+//     describe("Subtraction function", () => {
         
-    }); 
-    describe("Multiply function", () => {
+//     }); 
+//     describe("Multiply function", () => {
         
-    }); 
-    describe("Division function", () => {
+//     }); 
+//     describe("Division function", () => {
         
-    }); 
-})
+//     }); 
+// }); 
